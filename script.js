892caddcbf5c15ab0784fda769bedd69741fa98e
DorianI <>
2021-05-12 23:56:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var myAge = 15
+let guess = 0
+document.getElementById('enter').addEventListener('click', findAge)
+
+function findAge () { {
+    myAge = document.getElementById('age').value
+    guess = parseInt(myAge)
+   if (myAge = guess) {
+     document.getElementById('answer').innerHTML = 'You got it right!'
+   } else (guess < myAge) {
+     document.getElementById('answer').innerHTML = 'Your guess is too small, try again'
+   } else (guess < myAge) {
+     document.getElementById('answer').innerHTML = 'Your guess is too big, try again'
+   } 
+  }
+}
+
