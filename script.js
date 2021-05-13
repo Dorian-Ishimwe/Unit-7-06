26b046441396deab748f4a58fc805876801e3ec5
DorianI <>
2021-05-13 01:09:44 +0000

    changes of js

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ document.getElementById('enter').addEventListener('click', findAge)
 function findAge () { {
     myAge = document.getElementById('age').value
     guess = parseInt(myAge)
-   if (myAge = guess) {
+  if (myAge = guess) {
      document.getElementById('answer').innerHTML = 'You got it right!'
-   } else (guess < myAge) {
+    } else if (guess < myAge); {
      document.getElementById('answer').innerHTML = 'Your guess is too small, try again'
-   } else (guess < myAge) {
+    } else if (guess < myAge); {
      document.getElementById('answer').innerHTML = 'Your guess is too big, try again'
    } 
   }
